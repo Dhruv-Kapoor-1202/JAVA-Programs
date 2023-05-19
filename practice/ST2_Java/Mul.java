ef5041c51089176bc4cc21587bf7503244a62e41
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2023-05-19 09:42:46 +0530

    commit

--- practice/ST2_Java/Mul.java
+++ practice/ST2_Java/Mul.java
@@ -1,17 +1,31 @@
 import java.util.*;
 
+class Child implements Runnable {
+
+    Thread t;
+    String str;
+
+    Child(String str) {
+        this.str = str;
+        t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        int num = Integer.parseInt(str);
+        System.out.println("The given string is : " + num);
+    }
+}
+
 public class Mul {
     public static void main(String args[]) {
         Scanner obj = new Scanner(System.in);
 
-        System.out.print("Enter String:");
+        System.out.print("Enter number: ");
         String str = obj.nextLine();
 
-        StringBuilder s = new StringBuilder(str);
+        new Child(str);
 
-        StringBuffer a = new StringBuffer(str);
-
-        a.
-        s.
     }
 }
