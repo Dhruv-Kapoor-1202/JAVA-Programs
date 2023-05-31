b3009b8ec46841cab40b690838236b7db0ab5f23
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2023-05-31 14:09:45 +0530

    commit

--- May 2023/30th_May/ReadData.java
+++ May 2023/30th_May/ReadData.java
@@ -1,7 +1,43 @@
+// import java.sql.*;
+
+// public class ReadData {
+
+//     Connection con;
+//     PreparedStatement ps;
+//     ResultSet rs;
+
+//     ReadData() {
+//         try {
+//             Class.forName("com.mysql.jdbc.Driver");
+//             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dhruv", "User1", "Scootergangboi456!");
+//             System.out.println("Connection Established.");
+//             ps = con.prepareStatement("select * from noMe");
+//             rs = ps.executeQuery();
+//             // update / delete - executeUpdate();
+
+//             System.out.println("Read the table : noMe");
+//             System.out.println("\n\nUsername\tPassword");
+//             System.out.println("======================");
+
+//             while (rs.next()) {
+//                 System.out.print(rs.getString(1));
+//                 System.out.println("\t\t" + rs.getString(2));
+//             }
+
+//         } catch (Exception e) {
+//             System.out.println(e);
+//         }
+
+//     }
+
+//     public static void main(String args[]) {
+//         new ReadData();
+//     }
+// }
+
 import java.sql.*;
 
-public class ReadData {
-
+class ReadData {
     Connection con;
     PreparedStatement ps;
     ResultSet rs;
@@ -9,15 +45,16 @@ public class ReadData {
     ReadData() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dhruv", "User1", "Scootergangboi456!");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dhruv", "root", "Scootergangboi456!");
             System.out.println("Connection Established.");
-            ps = con.prepareStatement("select * from noMe");
-            rs = ps.executeQuery();
-            // update / delete - executeUpdate();
 
-            System.out.println("Read the table : noMe");
+            ps = con.prepareStatement("select * from login");
+            rs = ps.executeQuery();
+            // update/delete - executeUpdate();
+
+            System.out.println("Read the table : login");
             System.out.println("\n\nUsername\tPassword");
-            System.out.println("======================");
+            System.out.println("-----------------------------------");
 
             while (rs.next()) {
                 System.out.print(rs.getString(1));
@@ -27,7 +64,6 @@ public class ReadData {
         } catch (Exception e) {
             System.out.println(e);
         }
-
     }
 
     public static void main(String args[]) {
